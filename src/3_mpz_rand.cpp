a0a9569973915881605ecbd9409839adbdb106c0
dingxin <wdx7266@vip.qq.com>
2018-08-06 16:28:50 +0800

    update

--- src/3_mpz_rand.cpp
+++ src/3_mpz_rand.cpp
@@ -1,17 +1,18 @@
-#include<stdio.h>
-#include<gmp.h>
-#include<boost/random/random_device.hpp>
+#include <stdio.h>
+#include <gmp.h>
+#include <boost/random/random_device.hpp>
 
-int main( int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
     unsigned min_digits = 30;
     unsigned max_digits = 50;
-    unsigned quantity = 1000;   // How many numbers do you want?
-    unsigned sequence = 10;     // How many numbers before reseeding?
+    unsigned quantity = 1000; // How many numbers do you want?
+    unsigned sequence = 10;   // How many numbers before reseeding?
 
     mpz_t rmin;
     mpz_init(rmin);
-    mpz_ui_pow_ui(rmin, 10, min_digits-1);
+    mpz_ui_pow_ui(rmin, 10, min_digits - 1);
 
     mpz_t rmax;
     mpz_init(rmax);
@@ -25,14 +26,16 @@ int main( int argc, char *argv[]){
 
     boost::random::random_device rdev;
 
-    for( unsigned i = 0; i < quantity; i++){
+    for (unsigned i = 0; i < quantity; i++)
+    {
 
-        if(!(i % sequence)) 
-            gmp_randseed_ui(rstate, rdev.operator ()());
+        if (!(i % sequence))
+            gmp_randseed_ui(rstate, rdev.operator()());
 
-        do{
+        do
+        {
             mpz_urandomm(rnum, rstate, rmax);
-        }while(mpz_cmp(rnum, rmin) < 0);
+        } while (mpz_cmp(rnum, rmin) < 0);
 
         gmp_printf("%Zd\n", rnum);
     }
